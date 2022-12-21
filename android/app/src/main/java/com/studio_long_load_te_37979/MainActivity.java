0b27fd739198a13281fce94c2de12a5842a4e0b0
Crowdbotics <team@crowdbotics.com>
2022-12-21 20:50:06 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/studio_long_load_te_37979/MainActivity.java
@@ -0,0 +1,15 @@
+package com.studio_long_load_te_37979;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "studio_long_load_te_37979";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "studio_long_load_te_37979",
+  url: "https://studio_long_load_te_37979.botics.co",
+  api: "https://studio_long_load_te_37979.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
